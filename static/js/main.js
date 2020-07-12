e6babca29188c9f14e1f850927ad9b6408d4b7da
Toogii <dashdava@uw.edu>
2020-07-12 13:56:38 -0700

    Progressing

--- static/js/main.js
+++ static/js/main.js
@@ -2,18 +2,33 @@ $(document).ready(function() {
     console.log(moment());
     const today = moment().format("LL");
     $("#currentDay").text(today);
-    updateClock();
-
+    
+    function checkActivity() {
+        console.log("Focusing")
+    }
 
     var businessTimeArray = ["9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM","6:00 PM"];
     var timeOfTheDay = moment()
 
     function displayTimeBlocks () {
-        businessTimeArray.forEach(function(time,i) {
-            
+        $(".container").empty();
+        console.log(moment());
+
+        for (i=0;i<10;i++) {
             var rowDiv = $("<div>");
             rowDiv.attr("class", "row hour");
             var textArea = $("<textarea>");
+            // textArea.attr("onfocus", "function() test {console.log('focusing')}");
+            if (moment().isBefore(moment().hour(9+i))) {
+                textArea.attr("class","time-block future");
+            }
+            else if (moment().isSame(moment().hour(9+i))) {
+                textArea.attr("class","time-block present");
+            }
+            else {
+                textArea.attr("class","time-block past");
+
+            }
             // if (dateReal > dateOnCalendar) {
             //     textArea.attr("class", "time-block past");
             // }
@@ -27,23 +42,29 @@ $(document).ready(function() {
             // timeDesc.text(timeOfTheDay.hour(9+i).minutes(0).format("LT"));
             // rowDiv.append(timeDesc);
             var rowText = $("<div>");
-            rowText.text(timeOfTheDay.hour(9+i).format("h A"));
+            rowText.text(moment().hour(9+i).format("h A"));
             rowText.attr("class", "rowtext");
             rowDiv.append(rowText);
             rowDiv.append(textArea);
             rowDiv.append(saveButton);
             $(".container").append(rowDiv);
-        })
+        }
 
     }
 
     function updateClock() {
-        $(".clock").text(moment().format("hh:mm:ss A"));
+        $(".clock").text(moment().format("dddd") + " " + moment().format("LL") + " " + moment().format("LTS"));
         $("#currentDay").text(moment().format("LL"));
     }
-
-    setInterval(updateClock, 1000);
-
+    updateClock();
+    var clockInTheHeader = setInterval(updateClock, 1000);
+    // var timeBlockRefresh = setInterval(displayTimeBlocks, 1000);
     displayTimeBlocks();
+
+
+
+
+
+
 
 })
