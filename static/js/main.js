455964dcb98ea6713ac5a0f37193e16637a0e650
Toogii <dashdava@uw.edu>
2020-07-11 23:32:31 -0700

    Progressing

--- static/js/main.js
+++ static/js/main.js
@@ -1,25 +1,16 @@
 $(document).ready(function() {
     console.log(moment());
-    var m = moment();
-    $("#currentDay").text(moment().format("LL"));
- 
-    function getCurrentDate() {
-        m = moment();
-        console.log(m.format("LLL"))
-        return m.format("LLL");
-    }
+    const today = moment().format("LL");
+    $("#currentDay").text(today);
+    updateClock();
 
-    function getCurrentTime() {
-        m = moment().format("LLL");
-        return m
-    }
 
     var businessTimeArray = ["9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM","6:00 PM"];
     var timeOfTheDay = moment()
 
     function displayTimeBlocks () {
         businessTimeArray.forEach(function(time,i) {
-
+            
             var rowDiv = $("<div>");
             rowDiv.attr("class", "row hour");
             var textArea = $("<textarea>");
@@ -31,11 +22,12 @@ $(document).ready(function() {
             // }
             var saveButton = $("<button>");
             saveButton.attr("class", "saveBtn");
+            saveButton.text("Save");
             // var timeDesc = $("div").attr("class", "timeDesc");
             // timeDesc.text(timeOfTheDay.hour(9+i).minutes(0).format("LT"));
             // rowDiv.append(timeDesc);
             var rowText = $("<div>");
-            rowText.text(timeOfTheDay.hour(9+i).minutes(0).format("H A"));
+            rowText.text(timeOfTheDay.hour(9+i).format("h A"));
             rowText.attr("class", "rowtext");
             rowDiv.append(rowText);
             rowDiv.append(textArea);
@@ -44,6 +36,14 @@ $(document).ready(function() {
         })
 
     }
+
+    function updateClock() {
+        $(".clock").text(moment().format("hh:mm:ss A"));
+        $("#currentDay").text(moment().format("LL"));
+    }
+
+    setInterval(updateClock, 1000);
+
     displayTimeBlocks();
 
 })
