8647e139c1135f5d21e2cc413d9253bc5ce61e77
Toogii <dashdava@uw.edu>
2020-07-14 16:58:19 -0700

    Trying different way for Mobile support

--- static/js/main.js
+++ static/js/main.js
@@ -64,6 +64,85 @@ function showDates (today) {
         })
     }
 
+
+    function goToCalendar() {
+        $(".container").empty();
+    
+        let calendarAppDiv = $("<div>");
+        calendarAppDiv.attr("id", "calendar-app");
+        $(".container").append(calendarAppDiv);
+        $("#currentDay").text(moment($("#currentDay").text()).format("MMM YYYY"));
+        buildCalendar();
+        changeMonthBackground();
+    
+        function buildCalendar() {
+            let firstDay = moment($("#currentDay").text()).startOf('month');
+            let closestWeekend = moment($("#currentDay").text()).startOf('month').day(-1);
+            let lastDay = moment($("#currentDay").text()).endOf('month');
+            let gap = firstDay.diff(closestWeekend, 'days');
+            console.log(firstDay.format());
+            console.log(closestWeekend.format());
+            console.log(gap);
+            if (gap < 7) {
+                for (i=0;i<gap;i++) {
+                    calendarAppDiv.append(`<div class='day past-month' data-date='past-month'>${closestWeekend.format("ddd DD")}</div>`);
+                    closestWeekend.add(1, 'day');
+                }
+            }
+            while (!firstDay.isAfter(lastDay)) {
+                let day = firstDay.format("DD");
+                let month = firstDay.format("MMMM");
+                let year = firstDay.format("YYYY");
+                let dateChosen = month + " " + day + "," + " " + year;
+    
+                if (isWeekend(dateChosen)) {
+                    calendarAppDiv.append(`<div class="day weekend">${firstDay.format("ddd DD")}</div>`);
+                }
+                else if (isFutureDay(dateChosen)) {
+                    calendarAppDiv.append(`<div class="day future">${firstDay.format("ddd DD")}</div>`);
+                }
+                else if (isPresentDay(dateChosen)) {
+                    calendarAppDiv.append(`<div class="day present">${firstDay.format("ddd DD")}</div>`);
+                }
+                else if (isPastDay(dateChosen)) {
+                    calendarAppDiv.append(`<div class="day past">${firstDay.format("ddd DD")}</div>`);
+                }
+                else {
+                    calendarAppDiv.append(`<div class="day">${firstDay.format("ddd DD")}</div>`);
+                }
+                firstDay.add(1, 'day');
+            }
+        }
+        function jumpToDate(event) {
+            console.log(event.target.getAttribute("data-date"));
+            if (event.target.getAttribute("data-date") == "past-month") {
+                return;
+            }
+            let day = event.target.textContent.split(" ")[1];
+            let month = $("#currentDay").text().split(" ")[0];
+            let year = $("#currentDay").text().split(" ")[1];
+            let dateChosen = month + " " + day + "," + " " + year;
+            let date = moment(dateChosen);
+            showDates(date.format("LL"));
+    
+        }
+        $(".day").on("click", jumpToDate)
+    
+        function changeMonthBackground() {
+            if (isPresentMonth()) {
+                $("#currentDay").attr("class", "present");
+            }
+            else if (isPastMonth()) {
+                $("#currentDay").attr("class", "past");
+            }
+            else {
+                $("#currentDay").attr("class", "future");
+            }
+        }
+    
+    }
+    
+    
     
     function updateTextAreas(index) {
         let calendarDay = moment($("#currentDay").text()).format("LL");
